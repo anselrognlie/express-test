f226024f0a6d7f4435468451cd059b4683c44c52
Ansel Rognlie <ansel.rognlie@gmail.com>
2023-09-14 13:06:37 -0700

    remove morgan

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
 // create express server
 const express = require("express");
 // import express from "express";
-const morganMiddleware = require("../middlewares/morgan");
+// const morganMiddleware = require("../middlewares/morgan");
 const app = express();
 // const port = 3000;
 
 // serve static files
 // app.use(express.static("public"));
 app.use(express.json());
-app.use(morganMiddleware);
+// app.use(morganMiddleware);
 
 // define routes
 app.use("/api", require("../routes/template"));
