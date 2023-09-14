490370e64dc761356703b63a7c76e3920b7cbaa7
Ansel Rognlie <ansel.rognlie@gmail.com>
2023-09-14 13:02:57 -0700

    remove static files

--- api/index.js
+++ api/index.js
@@ -6,12 +6,12 @@ const app = express();
 // const port = 3000;
 
 // serve static files
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use(express.json());
 app.use(morganMiddleware);
 
 // define routes
-app.use("/template", require("../routes/template"));
+app.use("/api", require("../routes/template"));
 
 // start server
 // app.listen(port, () => console.log(`Server listening on port ${port}!`));
--- routes/template.js
+++ routes/template.js
@@ -1,16 +1,16 @@
 // create a route template
 const express = require("express");
 const router = express.Router();
-const path = require("path");
+// const path = require("path");
 
 router.use(express.json());
 
 // define routes
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+// router.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
 
-router.get("/api", (req, res) => {
+router.get("/", (req, res) => {
   res.json("Hello World!");
 });
 
